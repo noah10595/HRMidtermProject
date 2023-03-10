08a9e7e275508b7a75202f6c725bd6f783a98809
noah10595 <89624615+noah10595@users.noreply.github.com>
2023-03-10 03:52:56 -0500

    Initial Commit

---
+++ code_1/HumanresourcesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanresourcesproject.humanresourcesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanresourcesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
